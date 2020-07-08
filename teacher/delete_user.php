a2281469a28eab1fc2e7a8d75861badf961478bf
Sunee <suneekasem4@gmail.com>
2020-07-08 11:42:35 +0700

    12

---
+++ teacher/delete_user.php
@@ -0,0 +1,9 @@
+<?php
+include('../connect/connection.php');
+if (isset($_GET['del'])) {
+	$id_teacher = $_GET['del'];
+	mysqli_query($db, "DELETE FROM news WHERE id_teacher=$id_teacher");
+	$_SESSION['message'] = "ลบ!"; 
+	echo "<script>alert('ลบข้อมูลสำเร็จ');window.history.back();</script>";
+}
+?>
